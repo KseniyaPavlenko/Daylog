26e58c02e1aeb4bae00ff9089a72daf4090d61a0
KseniyaPavlenko <ksusha.pavlenko2002@gmail.com>
2023-03-12 12:24:14 +0200

    Displaying a Test Widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,8 @@ import 'package:daylog/cubits/event_detail/event_detail_cubit.dart';
 import 'package:daylog/cubits/event_list/event_list_cubit.dart';
 import 'package:daylog/cubits/me/me_cubit.dart';
 import 'package:daylog/pages/dashboard/dashboard_page.dart';
+import 'package:daylog/widgets/date_selector/date_selector_widget.dart';
+import 'package:daylog/widgets/widget_test/widget_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -21,6 +23,24 @@ void main() {
   setupDi();
   setupLog();
   runApp(App());
+  //testWidgets();
+}
+
+testWidgets() {
+  DateTime selectedDate = DateTime.now();
+  String previousDay =
+      selectedDate.subtract(const Duration(days: 1)).toString();
+  String nextDay = selectedDate.add(const Duration(days: 1)).toString();
+  const keyPreviosDay = Key('dateSelector_prev');
+  const keyNextDay = Key('dateSelector_next');
+  const dayText = Key('day');
+
+  runApp(WidgetTest(
+    child: DateSelectorWidget(
+      date: selectedDate,
+      onChange: (_) {},
+    ),
+  ));
 }
 
 class App extends StatefulWidget {
