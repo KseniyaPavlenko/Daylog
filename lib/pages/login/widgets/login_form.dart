1a59bee3c3fe00e6094b9118c59e07b249ca8735
KseniyaPavlenko <ksusha.pavlenko2002@gmail.com>
2023-04-05 17:17:46 +0300

    Fixed size of Login form

--- lib/pages/login/widgets/login_form.dart
+++ lib/pages/login/widgets/login_form.dart
@@ -22,45 +22,57 @@ class LoginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(bottom: 20, top: 10),
-          child: InputField(
-            key: const Key('UsernameInputFieldLogin'),
-            icon: const Icon(Icons.person_outline_sharp),
-            hint: "Username",
-            controller: usernameController,
-            obscure: false,
+        SizedBox(
+          width: 400,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 20, top: 10),
+            child: InputField(
+              key: const Key('UsernameInputFieldLogin'),
+              icon: const Icon(Icons.person_outline_sharp),
+              hint: "Username",
+              controller: usernameController,
+              obscure: false,
+            ),
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 5),
-          child: InputField(
-            key: const Key('PasswordInputFieldLogin'),
-            icon: const Icon(Icons.lock),
-            hint: "Password",
-            controller: passwordController,
-            obscure: true,
+        SizedBox(
+          width: 400,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 5),
+            child: InputField(
+              key: const Key('PasswordInputFieldLogin'),
+              icon: const Icon(Icons.lock),
+              hint: "Password",
+              controller: passwordController,
+              obscure: true,
+            ),
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.only(left: 260),
-          child: SizedBox(
-            height: 40,
-            width: MediaQuery.of(context).size.width,
-            child: createAccButton,
+        SizedBox(
+          width: 400,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 260),
+            child: SizedBox(
+              height: 40,
+              width: MediaQuery.of(context).size.width,
+              child: createAccButton,
+            ),
           ),
         ),
         const SizedBox(height: 20),
-        Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20),
-          child: SizedBox(
-            height: 50,
-            width: MediaQuery.of(context).size.width,
-            child: DefaultFloatingActionButton(
-              onTap: loginUser,
-              isEnabled: isEnabled,
-              text: 'LOGIN',
-              key: const Key('LoginButton'),
+        SizedBox(
+          width: 400,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: SizedBox(
+              height: 50,
+              width: MediaQuery.of(context).size.width,
+              child: DefaultFloatingActionButton(
+                onTap: loginUser,
+                isEnabled: isEnabled,
+                text: 'LOGIN',
+                key: const Key('LoginButton'),
+              ),
             ),
           ),
         )
