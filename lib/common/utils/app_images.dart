1dc507fa0ac3b7d26e48c6b26b6c73b4e4799536
KseniyaPavlenko <ksusha.pavlenko2002@gmail.com>
2023-04-07 20:32:40 +0300

    Images

---
+++ lib/common/utils/app_images.dart
@@ -0,0 +1,6 @@
+abstract class AppImages {
+  static const String _wireframes = 'assets/images';
+  static const String androidApp = '$_wireframes/AppStore.png';
+  static const String iosApp = '$_wireframes/PlayMarket.png';
+  static const String webApp = '$_wireframes/web2.png';
+}
