dd82426d411b04ab5df10a318608efcbbd75a0ee
KseniyaPavlenko <ksusha.pavlenko2002@gmail.com>
2023-05-24 17:55:30 +0300

    Added days to events

--- lib/common/utils/days.dart
+++ lib/common/utils/days.dart
@@ -6,7 +6,9 @@ abstract class Days {
   static const int friday = 16; // 2^4
   static const int saturday = 32; // 2^5
   static const int sunday = 64; // 2^6
-  
+  static const int allDays =
+      monday | tuesday | wednesday | thursday | friday | saturday | sunday;
+
   static bool isMonday(int value) {
     return (value & monday) == monday;
     // 0001011
@@ -14,29 +16,71 @@ abstract class Days {
     // 0000001
     // = 0000001
   }
-  
+
   static bool isTuesday(int value) {
     return (value & tuesday) == tuesday;
   }
-  
+
   static bool isWednesday(int value) {
     return (value & wednesday) == wednesday;
   }
-  
+
   static bool isThursday(int value) {
     return (value & thursday) == thursday;
   }
-  
-  
+
   static bool isFriday(int value) {
     return (value & friday) == friday;
   }
-  
+
   static bool isSaturday(int value) {
     return (value & saturday) == saturday;
   }
-  
+
   static bool isSunday(int value) {
     return (value & sunday) == sunday;
   }
+
+  static int toggle(int value, int day) {
+    return value ^ day;
+    // XOR
+    // 0001011
+    // &
+    // 0000001
+    // = 0000001
+
+    // false true = true
+    // true false = true
+    // true true = false
+    // false false false
+  }
+
+  static bool isSelected(int value, int day) {
+    return (value & day) == day;
+  }
+
+  static bool isSelectedEveryDay(int value) {
+    return value ==
+        (monday | tuesday | wednesday | thursday | friday | saturday | sunday);
+  }
+
+  static const List<int> daysOfTheWeek = [
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday,
+    sunday
+  ];
+
+  static const Map<int, String> weekDayNames = {
+    monday: 'Monday',
+    tuesday: 'Tuesday',
+    wednesday: 'Wednesday',
+    thursday: 'Thursday',
+    friday: 'Friday',
+    saturday: 'Saturday',
+    sunday: 'Sunday'
+  };
 }
